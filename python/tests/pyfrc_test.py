d35888055cde4c2129fc32133885a9ea04b6c7f3
Caden Dalley <caden.dalley@icloud.com>
2026-01-18 12:02:11 -0700

    fixes

--- python/tests/pyfrc_test.py
+++ python/tests/pyfrc_test.py
@@ -2,5 +2,68 @@
     This test module imports tests that come with pyfrc, and can be used
     to test basic functionality of just about any robot.
 '''
+import typing
 
-from pyfrc.tests import *
+import pytest
+
+if typing.TYPE_CHECKING:
+    from pyfrc.test_support.controller import TestController
+
+
+def test_autonomous(control: "TestController"):
+    """Runs autonomous mode by itself"""
+
+    with control.run_robot():
+        # Run disabled for a short period
+        control.step_timing(seconds=0.5, autonomous=True, enabled=False)
+
+        # Run enabled for 15 seconds
+        control.step_timing(seconds=15, autonomous=True, enabled=True)
+
+        # Disabled for another short period
+        control.step_timing(seconds=0.5, autonomous=True, enabled=False)
+
+
+@pytest.mark.filterwarnings("ignore")
+def test_disabled(control: "TestController", robot):
+    """Runs disabled mode by itself"""
+
+    with control.run_robot():
+        # Run disabled + autonomous for a short period
+        control.step_timing(seconds=5, autonomous=True, enabled=False)
+
+        # Run disabled + !autonomous for a short period
+        control.step_timing(seconds=5, autonomous=False, enabled=False)
+
+
+@pytest.mark.filterwarnings("ignore")
+def test_operator_control(control: "TestController"):
+    """Runs operator control mode by itself"""
+
+    with control.run_robot():
+        # Run disabled for a short period
+        control.step_timing(seconds=0.5, autonomous=False, enabled=False)
+
+        # Run enabled for 15 seconds
+        control.step_timing(seconds=15, autonomous=False, enabled=True)
+
+        # Disabled for another short period
+        control.step_timing(seconds=0.5, autonomous=False, enabled=False)
+
+
+@pytest.mark.filterwarnings("ignore")
+def test_practice(control: "TestController"):
+    """Runs through the entire span of a practice match"""
+
+    with control.run_robot():
+        # Run disabled for a short period
+        control.step_timing(seconds=0.5, autonomous=True, enabled=False)
+
+        # Run autonomous + enabled for 15 seconds
+        control.step_timing(seconds=1, autonomous=True, enabled=True)
+
+        # Disabled for another short period
+        control.step_timing(seconds=0.5, autonomous=False, enabled=False)
+
+        # Run teleop + enabled for 2 minutes
+        control.step_timing(seconds=1, autonomous=False, enabled=True)
